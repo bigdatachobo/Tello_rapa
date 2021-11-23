055c430cc374bd47109f35f75fbb1e112b8efe98
bigdatachobo <bigdatachobo@gmail.com>
2021-11-23 15:30:22 +0900

    main

--- simply.py
+++ simply.py
@@ -1,1 +1,15 @@
-from djitellopy import tello
+from djitellopy import Tello
+
+# tello 생성자 생성
+tello = Tello()
+
+# tello 연결
+tello.connect()
+
+# tello 이륙
+tello.takeoff()
+
+# tello 착륙
+tello.land()
+
+pass
